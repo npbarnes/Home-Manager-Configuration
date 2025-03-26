a7fc8984ba5f61d0d8c81237eb4fba592148d6b9
Nathan Barnes <nathanpaulbarnes@gmail.com>
2025-03-25 20:56:03 -0500

    Initial Commit

---
+++ home.nix
@@ -0,0 +1,197 @@
+{ config, pkgs, ... }:
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "deck";
+  home.homeDirectory = "/home/deck";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    pkgs.micro-full
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # An out of store symlink can be to a writable location. That way, you
+    # can directly update e.g. ~/.vimrc and see changes immediately without
+    # requiring a `home-manager switch`.
+    ".bashrc".source = config.lib.file.mkOutOfStoreSymlink dotfiles/bashrc;
+    ".vimrc".source = config.lib.file.mkOutOfStoreSymlink dotfiles/vimrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/deck/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # See also home.file.".bashrc"
+    #EDITOR = "vim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    profiles.default.extensions = [
+      pkgs.vscode-extensions.bbenoist.nix
+    ];
+  };
+
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      search = {
+        force = true; # force is required. See home-manager documentation.
+        default = "ddg"; # DuckDuckGo
+      };
+    };
+    policies = {
+      Cookies = {
+        Behavior = "reject-tracker-and-partition-foreign";
+      };
+      DisableFormHistory = true;
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounds = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DisplayBookmarksToolbar = "never";
+
+      EnableTrackingProtection = {
+        Value = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+      FirefoxHome = {
+        Search = true;
+        TopSites = true;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = false;
+        SponsoredPocket = false;
+        Snippets = false;
+      };
+      FirefoxSuggest = {
+        WebSuggestions = true;
+        SponsoredSuggestions = false;
+        ImproveSuggest = false;
+      };
+      HardwareAcceleration = true;
+      HttpsOnlyMode = "enabled";
+      OfferToSaveLoginsDefault = false;
+      PictureInPicture.Enabled = false;
+      PopupBlocking.Default = true;
+      Preferences = {
+        "browser.tabs.warnOnClose" = {
+          Value = false;
+          Status = "user";
+        };
+      };
+      SearchSuggestEnabled = true;
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "normal_installed";
+          default_area = "navbar";
+          private_browsing = true;
+        };
+        "jetpack-extension@dashlane.com" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/dashlane/latest.xpi";
+          installation_mode = "normal_installed";
+          default_area = "navbar";
+          private_browsing = true;
+        };
+        "{2662ff67-b302-4363-95f3-b050218bd72c}" = { # Untrap for Youtube
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/untrap-for-youtube/latest.xpi";
+          installation_mode = "normal_installed";
+        };
+      }; # end ExtensionSettings
+    }; # end policies
+  }; # end programs.firefox
+
+  programs.git = {
+    enable = true;
+    userName = "Nathan Barnes";
+    userEmail = "nathanpaulbarnes@gmail.com";
+    ignores = [ "*.swp" ];
+    extraConfig = {
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "main";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      commit.verbose = true;
+      merge.conflictstyle = "zdiff3";
+    };
+  };
+}
