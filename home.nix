711a5abcf5edb1e889184ceba116e76760d815fa
Nathan Barnes <nathanpaulbarnes@gmail.com>
2025-04-10 20:44:19 -0500

    Condense config directory

--- home.nix
+++ home.nix
@@ -40,7 +40,10 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  home.file = let
+    configDir = "${config.home.homeDirectory}/.config/home-manager";
+  in
+  {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -58,9 +61,9 @@
     # '';
 
     ".bashrc".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/.config/home-manager/dotfiles/bashrc";
+      "${configDir}/dotfiles/bashrc";
     ".vimrc".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/.config/home-manager/dotfiles/vimrc";
+      "${configDir}/dotfiles/vimrc";
 
     # Setup a basic firefox .desktop file.
     ".local/share/applications/firefox.desktop".source = ./firefox.desktop;
