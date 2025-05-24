0c786f175275e54cd59d24696b063e48bacbf055
Nathan Barnes <nathanpaulbarnes@gmail.com>
2025-05-24 12:37:56 -0500

    Remove distracting comments

--- home.nix
+++ home.nix
@@ -1,73 +1,30 @@
 { config, pkgs, ... }:
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "deck";
   home.homeDirectory = "/home/deck";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
     pkgs.man
     pkgs.man-pages
     pkgs.man-pages-posix
     pkgs.jujutsu
-    pkgs.rWrapper.override {
+    (pkgs.rWrapper.override {
       packages = [
         pkgs.rPackages.languageserver
         pkgs.rPackages.flexplot
-      ]
-    }
-
+      ];
+    })
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = let
     configDir = "${config.home.homeDirectory}/.config/home-manager";
   in
   {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # An out of store symlink can be to a writable location. That way, you
-    # can directly update e.g. ~/.vimrc and see changes immediately without
-    # requiring a `home-manager switch`.
-    # ".vimrc".source = config.lib.file.mkOutOfStoreSymlink dotfiles/vimrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-
     ".bashrc".source = config.lib.file.mkOutOfStoreSymlink
       "${configDir}/dotfiles/bashrc";
     ".vimrc".source = config.lib.file.mkOutOfStoreSymlink
@@ -81,28 +38,9 @@
       "${configDir}/dotfiles/ssh/config";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/deck/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # See also home.file.".bashrc"
-    #EDITOR = "vim";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   programs.vscode = {
